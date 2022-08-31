b87cc411c2a62286e5715c586b88d0bfec8d2a0d
Aleksandar Horvat <108305750+aleksandarhorvat@users.noreply.github.com>
2022-08-31 17:28:40 +0200

    Add files via upload

---
+++ C#/ThreeFiles/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ThreeFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.cbSelectFile = new System.Windows.Forms.ComboBox();
+            this.gbChoice = new System.Windows.Forms.GroupBox();
+            this.rbAverage = new System.Windows.Forms.RadioButton();
+            this.rbMaxNumberOfAppearances = new System.Windows.Forms.RadioButton();
+            this.rbMax = new System.Windows.Forms.RadioButton();
+            this.lRezultat = new System.Windows.Forms.Label();
+            this.gbChoice.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.ItemHeight = 16;
+            this.lbNumbers.Location = new System.Drawing.Point(79, 112);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(120, 180);
+            this.lbNumbers.TabIndex = 0;
+            // 
+            // cbSelectFile
+            // 
+            this.cbSelectFile.FormattingEnabled = true;
+            this.cbSelectFile.Items.AddRange(new object[] {
+            "File1",
+            "File2",
+            "File3"});
+            this.cbSelectFile.Location = new System.Drawing.Point(78, 58);
+            this.cbSelectFile.Name = "cbSelectFile";
+            this.cbSelectFile.Size = new System.Drawing.Size(133, 24);
+            this.cbSelectFile.TabIndex = 1;
+            this.cbSelectFile.Text = "Select a file";
+            this.cbSelectFile.SelectedIndexChanged += new System.EventHandler(this.cbSelectFile_SelectedIndexChanged);
+            // 
+            // gbChoice
+            // 
+            this.gbChoice.Controls.Add(this.rbAverage);
+            this.gbChoice.Controls.Add(this.rbMaxNumberOfAppearances);
+            this.gbChoice.Controls.Add(this.rbMax);
+            this.gbChoice.Enabled = false;
+            this.gbChoice.Location = new System.Drawing.Point(313, 79);
+            this.gbChoice.Name = "gbChoice";
+            this.gbChoice.Size = new System.Drawing.Size(230, 115);
+            this.gbChoice.TabIndex = 4;
+            this.gbChoice.TabStop = false;
+            this.gbChoice.Text = "Choice";
+            // 
+            // rbAverage
+            // 
+            this.rbAverage.AutoSize = true;
+            this.rbAverage.Location = new System.Drawing.Point(6, 75);
+            this.rbAverage.Name = "rbAverage";
+            this.rbAverage.Size = new System.Drawing.Size(80, 20);
+            this.rbAverage.TabIndex = 2;
+            this.rbAverage.TabStop = true;
+            this.rbAverage.Text = "Average";
+            this.rbAverage.UseVisualStyleBackColor = true;
+            this.rbAverage.CheckedChanged += new System.EventHandler(this.rbAverage_CheckedChanged);
+            // 
+            // rbMaxNumberOfAppearances
+            // 
+            this.rbMaxNumberOfAppearances.AutoSize = true;
+            this.rbMaxNumberOfAppearances.Location = new System.Drawing.Point(6, 48);
+            this.rbMaxNumberOfAppearances.Name = "rbMaxNumberOfAppearances";
+            this.rbMaxNumberOfAppearances.Size = new System.Drawing.Size(217, 20);
+            this.rbMaxNumberOfAppearances.TabIndex = 1;
+            this.rbMaxNumberOfAppearances.TabStop = true;
+            this.rbMaxNumberOfAppearances.Text = "Max an number of appearances";
+            this.rbMaxNumberOfAppearances.UseVisualStyleBackColor = true;
+            this.rbMaxNumberOfAppearances.CheckedChanged += new System.EventHandler(this.rbMaxNumberOfAppearances_CheckedChanged);
+            // 
+            // rbMax
+            // 
+            this.rbMax.AutoSize = true;
+            this.rbMax.Location = new System.Drawing.Point(6, 21);
+            this.rbMax.Name = "rbMax";
+            this.rbMax.Size = new System.Drawing.Size(53, 20);
+            this.rbMax.TabIndex = 0;
+            this.rbMax.TabStop = true;
+            this.rbMax.Text = "Max";
+            this.rbMax.UseVisualStyleBackColor = true;
+            this.rbMax.CheckedChanged += new System.EventHandler(this.rbMax_CheckedChanged);
+            // 
+            // lRezultat
+            // 
+            this.lRezultat.AutoSize = true;
+            this.lRezultat.Location = new System.Drawing.Point(222, 221);
+            this.lRezultat.Name = "lRezultat";
+            this.lRezultat.Size = new System.Drawing.Size(44, 16);
+            this.lRezultat.TabIndex = 5;
+            this.lRezultat.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 356);
+            this.Controls.Add(this.lRezultat);
+            this.Controls.Add(this.gbChoice);
+            this.Controls.Add(this.cbSelectFile);
+            this.Controls.Add(this.lbNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbChoice.ResumeLayout(false);
+            this.gbChoice.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.ComboBox cbSelectFile;
+        private System.Windows.Forms.GroupBox gbChoice;
+        private System.Windows.Forms.RadioButton rbAverage;
+        private System.Windows.Forms.RadioButton rbMaxNumberOfAppearances;
+        private System.Windows.Forms.RadioButton rbMax;
+        private System.Windows.Forms.Label lRezultat;
+    }
+}
+
---
+++ C#/ThreeFiles/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreeFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbSelectFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbSelectFile.Text!= "Select a file")
+            {
+                StreamReader f = new StreamReader("../../Files/" + cbSelectFile.Text + ".txt");
+                lbNumbers.Items.Clear();
+                while(!f.EndOfStream)
+                {
+                    lbNumbers.Items.Add(f.ReadLine());
+                }
+                f.Close();
+                gbChoice.Enabled = true;
+                lRezultat.Text = "";
+                rbMax.Checked = rbMaxNumberOfAppearances.Checked = rbAverage.Checked = false;
+            }
+        }
+
+        private void rbMax_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbMax.Checked)
+            {
+                StreamReader f = new StreamReader("../../Files/" + cbSelectFile.Text + ".txt");
+                int max, x;
+                max = Convert.ToInt32(f.ReadLine());
+                while(!f.EndOfStream)
+                {
+                    x = Convert.ToInt32(f.ReadLine());
+                    if (x > max) max = x;
+                }
+                f.Close();
+                lRezultat.Text = "Max number is " + max;
+            }
+        }
+
+        private void rbMaxNumberOfAppearances_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbMaxNumberOfAppearances.Checked)
+            {
+                StreamReader f = new StreamReader("../../Files/" + cbSelectFile.Text + ".txt");
+                int max, x,bp;
+                max = Convert.ToInt32(f.ReadLine());
+                bp = 1;
+                while (!f.EndOfStream)
+                {
+                    x = Convert.ToInt32(f.ReadLine());
+                    if (x == max) bp++;
+                    else if(x>max)
+                    {
+                        max = x;
+                        bp = 1;
+                    }
+                }
+                f.Close();
+                lRezultat.Text = "The maximum number is " + max + " it appears " + bp +" time";
+            }
+        }
+
+        private void rbAverage_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbAverage.Checked)
+            {
+                StreamReader f = new StreamReader("../../Files/" + cbSelectFile.Text + ".txt");
+                int x, sum=0,number=0;
+                while (!f.EndOfStream)
+                {
+                    x = Convert.ToInt32(f.ReadLine());
+                    sum += x;
+                    number++;
+                }
+                f.Close();
+                double p = sum * 1.0 / number;
+                lRezultat.Text = "Average is " + p.ToString("0.00");
+            }
+        }
+    }
+}
