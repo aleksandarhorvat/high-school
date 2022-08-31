bb2c15ce3979007c68899073708d83bf90dde381
Aleksandar Horvat <108305750+aleksandarhorvat@users.noreply.github.com>
2022-08-31 13:46:31 +0200

    Add files via upload

---
+++ C#/Factorial/Factorial/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Factorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btConfirm = new System.Windows.Forms.Button();
+            this.tbFirst = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(86, 101);
+            this.lbResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 16);
+            this.lbResult.TabIndex = 3;
+            // 
+            // btConfirm
+            // 
+            this.btConfirm.Location = new System.Drawing.Point(32, 69);
+            this.btConfirm.Margin = new System.Windows.Forms.Padding(4);
+            this.btConfirm.Name = "btConfirm";
+            this.btConfirm.Size = new System.Drawing.Size(132, 28);
+            this.btConfirm.TabIndex = 2;
+            this.btConfirm.Text = "Confirm";
+            this.btConfirm.UseVisualStyleBackColor = true;
+            this.btConfirm.Click += new System.EventHandler(this.btConfirm_Click);
+            // 
+            // tbFirst
+            // 
+            this.tbFirst.Location = new System.Drawing.Point(32, 39);
+            this.tbFirst.Margin = new System.Windows.Forms.Padding(4);
+            this.tbFirst.Name = "tbFirst";
+            this.tbFirst.Size = new System.Drawing.Size(132, 22);
+            this.tbFirst.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 162);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btConfirm);
+            this.Controls.Add(this.tbFirst);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btConfirm;
+        private System.Windows.Forms.TextBox tbFirst;
+    }
+}
+
---
+++ C#/Factorial/Factorial/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btConfirm_Click(object sender, EventArgs e)
+        {
+            double s = 1, i;
+            double a = Convert.ToDouble(tbFirst.Text);
+            for (i = 1; i <= a; i++)
+            {
+                s = s * i;
+            }
+            lbResult.Text = Convert.ToString(s);
+        }
+    }
+}
