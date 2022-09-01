ca1cb86ba6f787e6f3ef91a47793fb29077ee273
Aleksandar Horvat <108305750+aleksandarhorvat@users.noreply.github.com>
2022-09-01 13:38:53 +0200

    Add files via upload

---
+++ C#/IpAddresses/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace IpAddresses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btDisplay_Click(object sender, EventArgs e)
+        {
+            IPHostEntry host;
+            string localIP;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                localIP = ip.ToString();
+                listBox1.Items.Add(localIP);
+            }
+        }
+    }
+}
